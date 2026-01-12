7dae38bd31da97816e5122f30b2d74a3bb509884
Abhishek Shrestha <19162467+shresthabhishek@users.noreply.github.com>
2026-01-11 21:04:27 -0500

    Create package SentimentAnalysis

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def sentiment_analyzer(text_to_analyze):
     url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
@@ -6,4 +7,10 @@ def sentiment_analyzer(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
     
-    return response.text
+    formated_resp = json.loads(response.text)
+
+    label = formated_resp['documentSentiment']['label']
+    score = formated_resp['documentSentiment']['score']
+    
+    r = {'label': label, 'score': score}
+    return r
